0a248ee3e1d0b1dc2d0dac1c26f0dbfc91ed337f
Christian-Kern-S <christiankernp@gmai.com>
2025-11-25 14:54:55 -0300

    ensure Service returns AuthenticationError when LoadFacebookUserApi returns undefined

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,22 +1,13 @@
-/** @type {import('jest').Config} */
-const config = {
+module.exports = {
   testEnvironment: 'node',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: './tsconfig.jest.json'
-      }
-    ]
-  }
-  ,
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.jest.json' }]
+  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   }
 }
-
-export default config
